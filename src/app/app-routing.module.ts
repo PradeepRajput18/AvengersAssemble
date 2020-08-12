2e25d86c0a8e68df9bee7191279fe2e3a16ff6c2
pradeep <rekkapallipradeepG@gmail.com>
2020-08-12 13:27:04 +0530

    done authpages,pagenot, homepage partially

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './basic/home/home.component';
+import { PagenotfoundComponent } from './error/pagenotfound/pagenotfound.component';
+import { AboutusComponent } from './basic/aboutus/aboutus.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:"full"},
+  {path:"login",component:LoginComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"home",component:HomeComponent},
+  {path:"aboutus",component:AboutusComponent},
+  {path:"**",component:PagenotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './basic/home/home.component';
+import { ProductComponent } from './basic/product/product.component';
+import { TopnavComponent } from './basic/topnav/topnav.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { PagenotfoundComponent } from './error/pagenotfound/pagenotfound.component';
+import { SidenavComponent } from './profile/sidenav/sidenav.component';
+import { AboutusComponent } from './basic/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    TopnavComponent,
+    LoginComponent,
+    SignupComponent,
+    PagenotfoundComponent,
+    SidenavComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
